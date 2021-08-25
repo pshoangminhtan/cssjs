de6db2eb8c92f5d3062acf22e67b328c22ca3faf
pshoangminhtan <89521473+pshoangminhtan@users.noreply.github.com>
2021-08-25 20:35:57 +0700

    Update popupan.js

--- popupan.js
+++ popupan.js
@@ -1,7 +1,9 @@
 
-	var url_popup = 'https://www.phunsuong.org/';
-
-      //<![CDATA[
+	
+var i=Math.floor(Math.random() * 3);
+	var urls=["https://www.phunsuong.org/","https://www.facebook.com/C%C3%B4ng-ty-TNHH-TMDV-Ho%C3%A0ng-Minh-T%C3%A2n-1588382177855498","https://www.youtube.com/watch?v=KjhW9F4xoBk"];
+	
+	var url_popup = urls[i];      //<![CDATA[
 	  // var url_ppp = 'https://www.phunsuong.org/';
       function addEvent(obj, eventName, func){
         if (obj.attachEvent)
